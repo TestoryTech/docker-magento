c2f82c55a756e754becf87893ccecfb58779f9a4
Gera Weiss <gera.weiss@gmail.com>
2022-05-17 23:43:22 +0300

    Update populate_db.sh

--- 2/debian-10/rootfs/populate_db.sh
+++ 2/debian-10/rootfs/populate_db.sh
@@ -8,5 +8,6 @@ expect -exact "Password: "
 send -- "e7c36d03a98661b8a69aa2af2687702e\r"
 expect -exact "Do you want to store credentials for repo.magento.com in /bitnami/magento/var/composer_home/auth.json ? \[Yn\] "
 send -- "\r"
-spawn magento setup:upgrade
+expect eof
+magento setup:upgrade
 expect eof
